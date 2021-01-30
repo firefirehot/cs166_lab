3e9e7c3e1e746b9261ddeb4a5f09f1c97bdaa860
Robert Wyatt <rwyat002@wch132-12.cs.ucr.edu>
2021-01-29 22:14:48 -0800

    trying to remove erros from fastFile2

--- fastFile2.sql
+++ fastFile2.sql
@@ -16,31 +16,31 @@ DROP TABLE IF EXISTS Songs;
 DROP TABLE IF EXISTS Album;
 
 
-CREATE TABLE Professor (snn CHAR(11), Name text, age INTEGER, rank INTEGER, specialty text,
+CREATE TABLE Professor (ssn CHAR(11), Name text, age INTEGER, rank INTEGER, specialty text,
 	PRIMARY KEY(ssn)
 	);
 CREATE TABLE Dept (dno CHAR(11), dname text, office text, ssn CHAR(11),
 	PRIMARY KEY(dno),
-	FOREIGN KEY (ssn) REFERENCES Professor(ssn) ON DELETE SET DEFAULT "School Board"
+	FOREIGN KEY (ssn) REFERENCES Professor(ssn) ON DELETE SET NULL
 	);
 CREATE TABLE Project (pno CHAR(11), sponsor text, start_date text, end_date text, budget float, prof_superviser CHAR(11),
  	PRIMARY KEY(pno),
 	FOREIGN KEY (ssn) REFERENCES Professor(ssn) ON DELETE CASCADE,
 	FOREIGN KEY (prof_superviser) REFERENCES Professor(SSN) ON DELETE CASCADE 
 	);
-CREATE TABLE Graduate (SSN CHAR(11), advisor CHAR(11), name text, age INTEGER, deg_pg text, prof_superviser CHAR(11),
+CREATE TABLE Graduate (SSN CHAR(11), advisor CHAR(11), name text, age INTEGER, deg_pg text, prof_superviser CHAR(11), dno CHAR(11),
 	PRIMARY KEY(SSN),
-	FOREIGN KEY (dno) REFERENCES Dept(dno) ON DELETE SET DEFAULT "lost student",
-	FOREIGN KEY (advisor) REFERENCES Graduate(SSN) ON DELETE SET DEFAULT "next student",
-	FOREIGN KEY (prof_superviser) REFERENCES Professor(SSN) ON DELETE SET DEFAULT "New superviser"
+	FOREIGN KEY (dno) REFERENCES Dept(dno) ON DELETE SET NULL,
+	FOREIGN KEY (advisor) REFERENCES Graduate(SSN) ON DELETE SET NULL,
+	FOREIGN KEY (prof_superviser) REFERENCES Professor(SSN) ON DELETE SET NULL
 	);
 CREATE TABLE work_in(ssn CHAR(11), pno CHAR(11),
-	PRIMARY KEY(ssn,pno),
+	PRIMARY KEY(ssn, pno),
 	FOREIGN KEY (ssn) REFERENCES Professor(ssn),
 	FOREIGN KEY (pno) REFERENCES Project(pno)
 	);
-CREATE TABLE work_dept(snn CHAR(11), dno CHAR(11), time_pc float,
-	PRIMARY KEY(ssn,dno),
+CREATE TABLE work_dept(ssn CHAR(11), dno CHAR(11), time_pc float,
+	PRIMARY KEY(ssn, dno),
 	FOREIGN KEY (ssn) REFERENCES Professor(ssn),
 	FOREIGN KEY (dno) REFERENCES Dept(dno)
 	);
@@ -50,8 +50,8 @@ CREATE TABLE work_proj(pno CHAR(11), SSN CHAR(11), since float,
 	FOREIGN KEY (SSN) REFERENCES Graduate(SSN)
 	);
 
--If a projessor is deleted the project they was managing is deleted
--If a professor is deleted the department they was running is set to "school board" who takes over
+/*-If a projessor is deleted the project they was managing is deleted
+-If a professor is deleted the department they was running is set to NULL
 -If a professor is deleted the project they was supervising is deleted
 -If a professor is deleted the student they was superiving is set to "new superviser"
 -If a graduate is deleted the student they were advising is set to "next student"
@@ -59,6 +59,7 @@ CREATE TABLE work_proj(pno CHAR(11), SSN CHAR(11), since float,
 - manditory "one or more relations" must be defined elsewhere(in code). You can take it 
 -as "many" instead, with "one or more" enforced elsewhere. 
 -affected work_in, work_dept, advise, major, supervise.
+*/
 
 CREATE TABLE Musicians(ssn CHAR(11), name text,
 	PRIMARY KEY(ssn),
@@ -72,14 +73,14 @@ CREATE TABLE Songs(songId CHAR(11), title text, author text, albumIdentifier CHA
 	FOREIGN KEY (albumIdentifier) REFERENCES Album(albumIdentifier) ON DELETE CASCADE
 	);
 CREATE TABLE Instrument(instrId CHAR(11), dname text, key text,
-	PRIMARY KEY(instrId),
+	PRIMARY KEY(instrId)
 	);
 CREATE TABLE Place(address text,
-	PRIMARY KEY(address),
+	PRIMARY KEY(address)
 	);
 CREATE TABLE Telephone(phone_no text, address text,
 	PRIMARY KEY(address),
-	FOREIGN KEY (address) REFERENCES Place(address) ON DELETE CASCADE,
+	FOREIGN KEY (address) REFERENCES Place(address) ON DELETE CASCADE
 	);
 CREATE TABLE Plays(instrId CHAR(11), ssn CHAR(11),
 	PRIMARY KEY(instId, ssn),
@@ -92,12 +93,13 @@ CREATE TABLE Perform(ssn CHAR(11),songId CHAR(11),
 	FOREIGN KEY (songId) REFERENCES Songs(songId)
 	);
 Create Table Lives(ssn CHAR(11), address text,
-	PRIMARY KEY(ssn address);
+	PRIMARY KEY(ssn, address);
 	FOREIGN KEY (ssn) REFERENCES Musicians(ssn),
 	FOREIGN KEY (text) REFERENCES Place(text)
 	);
 
-
+/*
 -If an album is deleted all the songs that were in it are deleted.
 -If a can't be deleted from the database without first removing the albums that they produced.
 -If a place is deleted we also delete the telephone number associated with that place.
+*/
